2d82597bb5af0ea3ccc174f469834554372e2df7
kishor choithani <choithanisunit@gmail.com>
2020-09-22 08:24:01 +0530

    created dates day array

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ dates }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import moment from "moment";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      dates: Array.apply(null, Array(42)),
+    };
+  },
+  created() {
+    let startDay = moment()
+      .startOf("month")
+      .day();
+    let daysInMonth = moment().daysInMonth();
+    //"2016-02", "YYYY-MM"
+    if (startDay == 0) {
+      startDay = 7; //for monday as week starter
+    }
+    let count = 0;
+    this.dates = this.dates.map((day, i) => {
+      if (i >= startDay - 1 && i <= startDay - 1 + daysInMonth - 1) {
+        count = count + 1;
+        return count;
+      } else {
+        return;
+      }
+    });
+    //console.log(this.dates);
+  },
+};
 </script>
 
 <style>
