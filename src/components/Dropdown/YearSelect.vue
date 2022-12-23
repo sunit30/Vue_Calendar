885c5b721463c63d4f95917320828df41e8b262f
Sunit <56727564+sunit30@users.noreply.github.com>
2022-12-23 21:12:56 +0530

    updated years

--- src/components/Dropdown/YearSelect.vue
+++ src/components/Dropdown/YearSelect.vue
@@ -13,6 +13,9 @@
       <option value="2018">2018</option>
       <option value="2019">2019</option>
       <option value="2020">2020</option>
+      <option value="2021">2021</option>
+      <option value="2022">2022</option>
+      <option value="2023">2023</option>
     </select>
   </div>
 </template>
@@ -22,7 +25,7 @@ export default {
   name: "YearSelect",
   data() {
     return {
-      selectedYear: "2020",
+      selectedYear: "2022",
     };
   },
   methods: {
